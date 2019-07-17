011c006cffd23f5bae2010688f9eaf354ada231e
maxwilliamchao <maxwilliamchao@gmail.com>
2019-07-17 03:43:22 -0700

    implemented a hashmap

--- data_structures_implementation.py
+++ data_structures_implementation.py
@@ -1,6 +1,6 @@
-# Data Structures Implementation
+#  Data Structures Implementation
 
-#Array
+# Array
 from array import *
 
 arr = array('i', [5, 9, -2, 1, 0])
@@ -26,55 +26,43 @@ s = 0
 while s < len(squared_arr):
     print(squared_arr[s])
     s += 1
-#
-#n = int(input('Enter the length of the array '))
-#
-#for u in range(n):
-#    index = int(input('Enter the next value '))
-#    user_arr.append(index)
-#
-#print(user_arr)
-#
-#search_index = int(input('Enter the array value you are searching for to get its index '))
-#
-#k = 0
-#
-#for x in user_arr:
-#    if x == search_index:
-#        print(k)
-#        break
-#    k += 1
-#
-#
-#List
+
+# List
 ex_list = [1, 2, 3.14, 'a', ['b', 'c']]
 print('\nExample of a list in python \n' + str(ex_list))
 
-#Tuple
+# Tuple
 tuple1 = (1, -2)
 tuple2 = ('Adrian', '999-999-9999', 'example@email.com')
-print('\nExample of tuples\n' + str(tuple1) + '\n' + str(tuple2))
+print('\nExamples of tuples. Tuples are unmutable\n' + str(tuple1) + '\n' +
+      str(tuple2))
 
-#Set
-x = {m for m in range(8)}
-
-print()
-
-print('\nThis is a set.  Sets are unordered and contain unique elements\n' + str(x))
+# Set
+z = set([1, 2, 3, 3])
 
 print()
 
-#Dictionary
+print('''\nThis is a set 1, 2, 3, 3.  Sets are unordered and contain unique
+      elements, so there it contains only one 3. They are mutable and
+      operations like union, intersection and symmetric difference
+      can be performed\n''' + str(z))
+
+print()
+
+# Dictionary
 dictionary1 = {'pork': 22.50, 'beef': 31.25, 'chicken': 18.75}
-print('\nLike sets, dictionaries are unordered but instead, contain unique key-value pairs\n' + str(dictionary1))
+print('''\nLike sets, dictionaries are unordered but instead,
+      contain unique key-value pairs\n''' + str(dictionary1))
 
-#Linear Data Structures: List/Array, Linked List, Stack, Queue
+# Linear Data Structures: List/Array, Linked List, Stack, Queue
 
-#Linked List
+# Linked List
+
+
 class Node:
     def __init__(self, data):
         self.data = data
-        self.next = None #Make None default value for head
+        self.next = None # Make None default value for head
 
 def count_nodes(head):
     count = 1
@@ -84,6 +72,7 @@ def count_nodes(head):
         current = current.next
         count += 1
     return count
+
 
 class linked_list:
     def __init__(self):
@@ -127,10 +116,12 @@ llist.reverse_iterative()
 print()
 llist.show_nodes()
 
-#Stack
+# Stack
 
-#implemented as a class
+# implemented as a class
 print('\nStack')
+
+
 class Stack():
     def __init__(self):
         self.items = []
@@ -170,9 +161,11 @@ s.push('A')
 print(str(s.peek()) + ' <-- After adding an "A" peeks at the top of stack') 
 print()
 
-#Queue
+# Queue
 from collections import deque
 print('Queue')
+
+
 class Queue():
     def __init__(self):
         self.items = deque()
@@ -202,28 +195,31 @@ print(q.get_queue())
 print()
 
 
-#Binary Search Tree - used to store naturally hierarchial data like a file system. EX. Network routing algorithms
-#Node values are ordered, beginning after the root,  with the left child less than the parent and the right child greater than
-#The leftmost node should be the smallest, the root in the middle, and the rightmost the greatest
-#Olog(n) time complexity
+# Binary Search Tree - used to store naturally hierarchial data like a file system. EX. Network routing algorithms
+# Node values are ordered, beginning after the root,  with the left child less than the parent and the right child greater than
+# The leftmost node should be the smallest, the root in the middle, and the rightmost the greatest
+# Olog(n) time complexity
 print('BST')
+
+
 class Node:
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
 
+
 class Tree:
     def __init__(self):
         self.root = None
-    #add node
+    # add node
     def add(self, val):
         if(self.root == None):
             self.root = Node(val)
         else:
             self.add_node(self.root, val)
 
-    #node should not be null
+    # node should not be null
     def add_node(self, node, val):
         if(node.val < val):
             self.add_right(node, val)
@@ -275,14 +271,15 @@ tree.add(7)
 tree.add(9)
 tree.printTree()
 print()
-#Heap - Min/Max heaps have their min/max node at the root of a binary tree
-#parent nodes are always less than their children for a min heap and greater than for a max heap
-#it is complete and not unbalanced
-#heaps are one maximally efficient implementation of a priority queue data type
-#it has nothing to do with the memory pool from which dynamically allocated memory is allocated
-#O(1) time complexity
+# Heap - Min/Max heaps have their min/max node at the root of a binary tree
+# parent nodes are always less than their children for a min heap and greater than for a max heap
+# it is complete and not unbalanced
+# heaps are one maximally efficient implementation of a priority queue data type
+# it has nothing to do with the memory pool from which dynamically allocated memory is allocated
+# O(1) time complexity
 
 print('Max Heap')
+
 
 class Heap(object):
 
@@ -350,7 +347,7 @@ class Heap(object):
             else:
                 break
     
-    #perform O(N logN) sorting IN PLACE
+    # perform O(N logN) sorting IN PLACE
     def heapsort(self):
         for i in range(0,self.currentPosition + 1):
             temp = self.heap[0]
@@ -374,41 +371,109 @@ heap.insert(4)
 heap.heapsort()
 
 print()
-#Hashmap
-print('Hashmap')
+# Hashmap
+print('''\nHashmap /hash-table/associative array
+      which is a dictionary or dict() in python\n''')
+
+
+class HashMap:
+    def __init__(self):
+        # for example to demonstrate collision.
+        # in real life defaults to 64 or so
+        self.size = 6
+        self.map = [None] * self.size
+
+    def _get_hash(self, key):
+        hash = 0
+        for char in str(key): # example simple hashing function
+            hash += ord(char)
+        return hash % self.size
+
+    def add(self, key, value):
+        key_hash = self._get_hash(key) # the index value
+        key_value = [key, value] # what you want to insert into the cell
+
+        if(self.map[key_hash] is None): # checks if cell is empty
+            self.map[key_hash] = list([key_value]) # adds key-value pair list
+            return True
+        else:
+            for pair in self.map[key_hash]: # if the key exists update the val
+                if(pair[0] == key):
+                    pair[1] = value
+                    return True
+            self.map[key_hash].append(key_value) # else append key to list 
+            return True
+
+    def get(self, key):
+        key_hash = self._get_hash(key)
+        if(self.map[key_hash] is not None):
+            for pair in self.map[key_hash]: # iterates pairs in each cell
+                if(pair[0] == key):
+                    return pair[1] # to return val that matches key
+        return None
+
+    def delete(self, key):
+        key_hash = self._get_hash(key)
+        if(self.map[key_hash] is None):
+            return False
+        for i in range(0, len(self.map[key_hash])): #gets index
+            if(self.map[key_hash][i][0] == key): # locate item to be removed
+                self.map[key_hash].pop(i)
+                return True
+
+    def print(self):
+        print('---Phonebook---')
+        for item in self.map: # prints the non-None cells
+            if(item is not None):
+                print(str(item))
+
+h = HashMap()
+h.add('Bob', '567-8888')
+h.add('Ming', '293-6753')
+h.add('Ming', '333-8233') # overwrites old number with new
+h.add('Ankit', '293-8625')
+h.add('Aditya', '852-6551')
+h.add('Alicia', '632-4123')
+h.add('Mike', '567-2188')
+h.add('Aditya', '777-8888') # overwrite again
+h.print()
+h.delete('Bob')
+print('\nNow without Bob\n')
+h.print()
+print('Ming: ' + h.get('Ming'))
 
 
 
 
 
 
+# Graph
 
-print()
-#Graph
 class Vertex:
     def __init__(self, key):
         self.id = key
-        self.connectedTo = {}  #key is vertex obj, value is cost
+        self.connectedTo = {}  # key is vertex obj, value is cost
 
-    def addNeighbor(self, nbr, cost = None): #nbr is vertex obj
+    def addNeighbor(self, nbr, cost = None): # nbr is vertex obj
         self.connectedTo[nbr] = cost
 
-    #printing
+    # printing
     def __str__(self):
         return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
 
-    def getConnections(self):   #returns vertex objects list
+    def getConnections(self):   # returns vertex objects list
         return self.connectedTo.keys()
 
     def getId(self):
         return self.id
 
-    def getWeight(self, nbr):  #nbr vertex object
+    def getWeight(self, nbr):  # nbr vertex object
         return self.connectedTo[nbr]
+
 
 class Graph:
     def __init__(self):
-        self.vertList = {}  #dict of id key and vertex obj value
+        self.vertList = {}  # dict of id key and vertex obj value
         self.numVertices = 0
 
     def addVertex(self, key):
@@ -417,25 +482,25 @@ class Graph:
         self.vertList[key] = newVertex
         return newVertex
 
-    def getVertex(self, n): #returns Vertex object with id n
+    def getVertex(self, n): # returns Vertex object with id n
         if(n in self.vertList):
             return self.vertList[n]
         else:
             return None
 
-    def __contains__(self, n): #check if vertex object n in self
+    def __contains__(self, n): # check if vertex object n in self
         return n in self.vertList
 
 
-    #graph user will need to call this twice for undirected graph
-    def addEdge(self, f, t, cost = 0): #note nv not used
+    # graph user will need to call this twice for undirected graph
+    def addEdge(self, f, t, cost = 0): # note nv not used
         if f not in self.vertList:
             nv = self.addVertex(f)
         if t not in self.vertList:
             nv = self.addVertex(t)
         self.vertList[f].addNeighbor(self.vertList[t], cost)
 
-    def getVertices(self): #gets list of ids, not vertex objects
+    def getVertices(self): # gets list of ids, not vertex objects
         return self.vertList.keys()
 
     def __iter__(self):
@@ -444,7 +509,7 @@ class Graph:
 
 print()
 print('A Simple graph')
-#create a simple graph without direction
+# create a simple graph without direction
 def add_both_edges(g, key1, key2):
     g.addEdge(key1, key2)
     g.addEdge(key2, key1)
@@ -456,14 +521,14 @@ add_both_edges(g, 'Mary', 'Joe')
 add_both_edges(g, 'Joe', 'Tom')
 g.addVertex('Sally')
 
-#printing
+# printing
 for from_id in g.getVertices():
     t = tuple(v.id for v in g.getVertex(from_id).connectedTo.keys())
     print(f'edge: {from_id} to {t}')
 
 print()
 
-#create a directed graph
+# create a directed graph
 print('Directed graph')
 h = Graph()
 h.addEdge('V0', 'V1', 5)
@@ -477,7 +542,7 @@ h.addEdge('V5', 'V2', 1)
 h.addEdge('V5', 'V4', 8)
 
 
-#printing
+# printing
 for from_id in h.getVertices():
     t = tuple(f'{v.id}:{cost}' for v, cost in h.getVertex(from_id).connectedTo.items())
     print(f'edge: {from_id} to : {t}')
@@ -490,7 +555,7 @@ print()
 
 
 
-#Matrix
+# Matrix
 print('Matrix math')
 from numpy import *
 
@@ -518,25 +583,14 @@ m = matrix('1 2 3; 4 5 6')
 n = matrix('1 2 3; 3 4 42')
 o = matrix('1 0; 0 1; 0 0')
 
-print(m)
-print()
-print(n)
-print()
-print(o)
+print(str(m) + ' matrix m\n')
+print(str(n) + ' matrix n\n')
+print(str(o) + ' matrix o\n')
+print(str(m + n) + ' matrix m + matrix n\n')
 
-print()
-
-print(str(m) + ' matrix m')
-
-print()
-
-print(str(m + n) + 'matrix n')
-
-print()
-
-#[1*1+2*0+3*0= 1 1*0+2*1+3*0= 2]    Each index in [1stRow*1stCol 1stRow*2ndCol]
-#[3*1+4*0+42*0= 3 3*0+4*1+42*0= 4]                [2ndRow*1stCol 2ndRow*2ndCol]
-print(str(n * o) + ' matrix n * matix o\n')
+# [1*1+2*0+3*0= 1 1*0+2*1+3*0= 2]    Each index in [1stRow*1stCol 1stRow*2ndCol]
+# [3*1+4*0+42*0= 3 3*0+4*1+42*0= 4]                [2ndRow*1stCol 2ndRow*2ndCol]
+print(str(n * o) + ' matrix n * matrix o\n')
 print('''[1*1+2*0+3*0= 1 1*0+2*1+3*0= 2]    Each index in [1stRow*1stCol 1stRow*2ndCol]
 [3*1+4*0+42*0= 3 3*0+4*1+42*0= 4]                [2ndRow*1stCol 2ndRow*2ndCol]
 ''')
